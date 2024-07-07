942c664617f3aa41d6ee8928a2281f5a7884d886
xiaoqian2024 <zhouxiaoqian88@gmail.com>
2024-07-07 11:51:39 +0800

    Site updated: 2024-07-07 11:51:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/这是一篇新的文章/","2024/07/07/hello-world/"];function toRandomPost(){
+var posts=["2024/07/07/这是一好的博文/","2024/07/07/hello-world/","2024/07/07/这是一篇新的文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
